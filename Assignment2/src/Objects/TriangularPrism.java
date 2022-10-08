1ed387388b48b8f3b370691b2021db93caaebb1d
Justin Gil <113378840+Jjuicetin@users.noreply.github.com>
2022-10-08 16:58:51 -0600

    Update TriangularPrism.java

--- Assignment2/src/Objects/TriangularPrism.java
+++ Assignment2/src/Objects/TriangularPrism.java
@@ -3,39 +3,45 @@ package Objects;
 public class TriangularPrism extends Shape {
 	// Attributes 
 		private double side;
-		// Constructor
-		public TriangularPrism()
-		{
-			
-		}
+		/*
+		 * Initializes a new instance of TriangularPrism object
+		 */	
+		public TriangularPrism(){}
+		/*
+		 * Initializes a new instance of TriangularPrism object
+		 * @param shape name
+		 * @param double value of height
+		 * @param double value of side
+		 */	
 		public TriangularPrism(String shape, double height, double side)
 		{
 			super(shape, height);
 			this.side = side;
 		}
 		
-		//getters and setters
-		
+		/* 
+		 * @return returns the side of this object 
+		 * */
 		public double getSide()
 		{
 			return side;
 		}
-		
+		/* 
+		 * set new value to side of this object
+		 * */
 		public void setSide()
 		{
 			this.side = side;
 		}
-		
-		
-		
-	
-		
-		
+
+		/* @return volume of Triangular Prism object */
 		@Override
 		public double calcVolume() {
 			// TODO Auto-generated method stub
 			return  ((side * side * Math.sqrt(3.0)) / 4) * this.getHeight();
 		}
+		
+		/* @return base area of triangular Prism object */
 		@Override
 		public double calcBaseArea() {
 			// TODO Auto-generated method stub
