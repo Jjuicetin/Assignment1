7439206c4e5fe067b8ed175bf3479128ff775b27
Justin Gil <113378840+Jjuicetin@users.noreply.github.com>
2022-10-08 16:59:25 -0600

    Update SquarePrism.java

--- Assignment2/src/Objects/SquarePrism.java
+++ Assignment2/src/Objects/SquarePrism.java
@@ -3,38 +3,50 @@ package Objects;
 public class SquarePrism extends Shape {
 	// Attributes 
 	private double side;
-	// Constructor
+
+	/*
+	 * Initializes a new instance of SquarePrism object
+	 */	
 	public SquarePrism()
 	{
 		
 	}
+	/*
+	 * Initializes a new instance of SquarePrism object
+	 * @param shape name
+	 * @param double value of height
+	 * @param double value of side
+	 */	
 	public SquarePrism(String shape, double height, double side)
 	{
 		super(shape, height);
 		this.side = side;
 	}
 	
-	//getters and setters
 	
+	/* 
+	 * @return returns the side of this object 
+	 * */
 	public double getSide()
 	{
 		return side;
 	}
-	
+	/* 
+	 * set new value to side of this object
+	 * */
 	public void setSide()
 	{
 		this.side = side;
 	}
 	
-	
-	
-
-
+	/* @return base area of Square Prism object */
 	@Override
 	public double calcBaseArea() {
 		
 		return side * side;
 	}
+	
+	/* @return volume of Square Prism object */
 	@Override
 	public double calcVolume() {
 		
